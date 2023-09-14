d93ef6b029ac7afec034a8960a438faa0a97aa06
meursault00 <inakibengolea@gmail.com>
2023-09-14 20:15:07 -0300

    Send email to post suscribers when commented
    Uses Thymeleaf to send emails with http format to post suscribers and its owner each time a new comment is made on said post.

--- services/src/main/java/ar/edu/itba/paw/services/CommentServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/CommentServiceImpl.java
@@ -3,6 +3,7 @@ package ar.edu.itba.paw.services;
 import ar.edu.itba.paw.interfaces.persistence.CommentDao;
 import ar.edu.itba.paw.interfaces.services.CommentService;
 import ar.edu.itba.paw.interfaces.services.EmailService;
+import ar.edu.itba.paw.interfaces.services.NeighborService;
 import ar.edu.itba.paw.interfaces.services.PostService;
 import ar.edu.itba.paw.models.Comment;
 import ar.edu.itba.paw.models.Neighbor;
@@ -21,12 +22,14 @@ public class CommentServiceImpl implements CommentService {
     private final CommentDao commentDao;
     private final EmailService emailService;
     private final PostService ps;
+    private final NeighborService ns;
 
     @Autowired
-    public CommentServiceImpl(final CommentDao commentDao, EmailService emailService, PostService ps) {
+    public CommentServiceImpl(final CommentDao commentDao, EmailService emailService, PostService ps, NeighborService ns){
         this.commentDao = commentDao;
         this.emailService = emailService;
         this.ps = ps;
+        this.ns = ns;
     }
 
     @Override
@@ -45,8 +48,17 @@ public class CommentServiceImpl implements CommentService {
             variables.put("name", neighbor.getName());
             variables.put("postTitle", post.getTitle());
             variables.put("postPath", "http://localhost:8080/posts/" + post.getPostId());
-
             emailService.sendMessageUsingThymeleafTemplate(neighbor.getMail(), "New comment", null, variables);
+
+            for(Neighbor n : ns.getNeighborsSubscribedByPostId(postId)) {
+                Map<String, Object> vars = new HashMap<>();
+                vars.put("name", n.getName());
+                vars.put("postTitle", post.getTitle());
+                vars.put("postPath", "http://localhost:8080/posts/" + post.getPostId());
+                System.out.println(vars);
+                emailService.sendMessageUsingThymeleafTemplate(n.getMail(), "New comment", null, vars);
+            }
+
 
         } catch (MessagingException e) {
             // Handle the exception, e.g., log it
