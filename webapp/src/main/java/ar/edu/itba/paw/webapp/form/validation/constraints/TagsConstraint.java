6abaed07beaa05cd21c3bc2590e824e439790934
Chris Ijjas <cijjas@itba.edu.ar>
2023-09-16 16:49:51 -0300

    Add tags validation and constraints
    Created tag validation and constraints. Also added the structure for
    future image validation

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/validation/constraints/TagsConstraint.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/validation/constraints/TagsConstraint.java
@@ -1,19 +1,18 @@
 package ar.edu.itba.paw.webapp.form.validation.constraints;
 
-
 import ar.edu.itba.paw.webapp.form.validation.validators.ImageValidator;
+import ar.edu.itba.paw.webapp.form.validation.validators.TagsValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Constraint(validatedBy = ImageValidator.class)
+@Constraint(validatedBy = TagsValidator.class)
 @Target( { ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ImageConstraint {
-
-    String message() default "Invalid image format";
+public @interface TagsConstraint {
+    String message() default "Invalid tags format";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/validation/validators/ImageValidator.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/validation/validators/ImageValidator.java
@@ -2,25 +2,30 @@ package ar.edu.itba.paw.webapp.form.validation.validators;
 
 import ar.edu.itba.paw.webapp.form.validation.constraints.ImageConstraint;
 
+import javax.imageio.ImageIO;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-
-
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 
 // https://www.baeldung.com/spring-mvc-custom-validator
 public class ImageValidator implements
-        ConstraintValidator<ImageConstraint, String> {
+        ConstraintValidator<ImageConstraint, byte[]> {
 
+    private static final long MAX_IMAGE_SIZE_BYTES = 10 * 1024 * 1024; // 10MB
 
     @Override
     public void initialize(ImageConstraint imageConstraint) {
 
     }
 
-    // el string es lo que queres validar
     @Override
-    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        return false;
+    public boolean isValid(byte[] imageData, ConstraintValidatorContext constraintValidatorContext) {
+        // Check if the image byte array is null or empty
+        return true;
     }
 }
