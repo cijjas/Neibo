cf445bd759809839cd151043a5de713c6ae4b960
JoacoGirod <joaquingirod@gmail.com>
2023-11-01 18:36:10 -0300

    Fix Bugs in Marketplace

--- persistence/src/test/java/ar/edu/itba/paw/persistence/MainEntitiesTests/ProductDaoImplTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/MainEntitiesTests/ProductDaoImplTest.java
@@ -182,8 +182,8 @@ public class ProductDaoImplTest {
         long uKey1 = testInserter.createUser(MAIL1, nhKey);
         long uKey2 = testInserter.createUser(MAIL2, nhKey);
         long dKey2 = testInserter.createDepartment(Departments.ELECTRONICS);
-        long pKey1 = testInserter.createProduct(iKey, iKey, iKey, uKey1, uKey2, dKey2);
-        long pKey2 = testInserter.createProduct(iKey, iKey, iKey, uKey2, uKey1, dKey2);
+        long pKey1 = testInserter.createProduct(iKey, iKey, iKey, uKey1, null, dKey2);
+        long pKey2 = testInserter.createProduct(iKey, iKey, iKey, uKey2, null, dKey2);
 
         // Exercise
         List<Product> products = productDao.getProductsSelling(uKey1);
