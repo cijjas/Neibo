49db3380d22b3f6e8a247a6ab1f8a569673726a4
flopezmenardi <flopezmenardi@itba.edu.ar>
2023-11-02 00:35:39 -0300

    Add duplicate email validation
    Validation for an email already in use in sign up and worker sign up forms

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/validation/validators/DuplicateEmailValidator.java
@@ -0,0 +1,28 @@
+package ar.edu.itba.paw.webapp.form.validation.validators;
+
+import ar.edu.itba.paw.interfaces.services.UserService;
+import ar.edu.itba.paw.webapp.form.validation.constraints.EmailConstraint;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class DuplicateEmailValidator implements ConstraintValidator<EmailConstraint, String> {
+    @Autowired
+    UserService userService;
+
+    @Override
+    public void initialize(EmailConstraint emailConstraint) {
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        if(userService.findUserByMail(email).isPresent()) {
+            constraintValidatorContext.disableDefaultConstraintViolation();
+            constraintValidatorContext.buildConstraintViolationWithTemplate("Email already in use")
+                    .addConstraintViolation();
+            return false;
+        }
+        return true;
+    }
+}
