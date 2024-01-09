bafafe3f4d04f621d2a2c670003add17b4060b45
flopezmenardi <flopezmenardi@itba.edu.ar>
2024-01-09 20:38:57 -0300

    Create rest of exception mappers

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/mappers/MailingExceptionMapper.java
@@ -0,0 +1,31 @@
+package ar.edu.itba.paw.webapp.mappers;
+
+import ar.edu.itba.paw.interfaces.exceptions.MailingException;
+import ar.edu.itba.paw.models.ApiErrorDetails;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class MailingExceptionMapper  implements ExceptionMapper<MailingException> {
+
+    @Context
+    private UriInfo uriInfo;
+
+    @Override
+    public Response toResponse(MailingException exception) {
+        Response.Status status = Response.Status.INTERNAL_SERVER_ERROR;
+
+        ApiErrorDetails errorDetails = new ApiErrorDetails();
+        errorDetails.setStatus(status.getStatusCode());
+        errorDetails.setTitle(status.getReasonPhrase());
+        errorDetails.setMessage(exception.getMessage());
+        errorDetails.setPath(uriInfo.getAbsolutePath().getPath());
+
+        return Response.status(status).entity(errorDetails).type(MediaType.APPLICATION_JSON).build();
+    }
+}
