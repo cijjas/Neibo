911dd1017652c6f798cb2074c840b9bf18792dcb
meursault00 <inakibengolea@gmail.com>
2024-01-19 20:34:34 -0300

    Fix bug

--- webapp/front/front/src/app/app.component.ts
+++ webapp/front/front/src/app/app.component.ts
@@ -1,54 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { forkJoin, Observable } from 'rxjs';
-import { switchMap, map } from 'rxjs/operators';
-import { PostForm } from './postForm';
-import { PostService } from './post.service';
-import { ImageForm } from './imageForm';
-import { ImageService } from './image.service';
-import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core'
+import { Post } from './post'
+import { PostService } from './post.service'
+import { Image } from './image'
+import { ImageService } from './image.service'
+import { HttpErrorResponse } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] // Correct the property name to 'styleUrls'
+  styleUrl: '../../../../src/main/webapp/resources/css/home.css'
 })
 export class AppComponent implements OnInit {
-  title = 'neibo';
-  public postList: PostForm[] = [];
+  title = 'neibo'
+  public postList: Post[] = []
 
-  constructor(private postService: PostService, private imageService: ImageService) {}
+  constructor(private postService: PostService){}
 
   ngOnInit() {
-    this.getPosts();
+    this.getPosts()
   }
 
   public getPosts(): void {
-    this.postService.getPosts(1, 'FEED', [], 'none', 1, 1, 10).pipe(
-      switchMap((response: PostForm[]) => {
-        return forkJoin(
-          response.map(post =>
-            this.imageService.getImage(this.extractImageIdFromUri(post.imageFile)).pipe(
-              map(image => ({ post, image }))
-            )
-          )
-        );
-      })
-    ).subscribe(
-      (postImagePairs: { post: PostForm, image: ImageForm }[]) => {
-        // Assuming you have a property for postImagePairs
-        this.postList = postImagePairs.map(pair => pair.post);
+    this.postService.getPosts(1, "FEED", [], "none", 1, 1, 10).subscribe(
+      (response: Post[]) => {
+        this.postList = response
       },
       (error: HttpErrorResponse) => {
-        alert(error.message);
+        alert(error.message)
       }
-    );
+    )
   }
 
-  // Add a method to extract the image ID from the URI
-  private extractImageIdFromUri(uri: string): number {
-    // Implement the logic to extract the image ID from the URI
-    // For example, extract the ID from the end of the URI
-    const parts = uri.split('/');
-    return +parts[parts.length - 1];
-  }
 }
