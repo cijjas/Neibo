542367edd9d200565dca44726fbca8bb7ead68fb
Chris Ijjas <cijjas@itba.edu.ar>
2024-01-25 17:45:55 -0300

    Make post component with missing channel string

--- webapp/front/front/src/app/components/post/post.component.ts
+++ webapp/front/front/src/app/components/post/post.component.ts
@@ -1,12 +1,59 @@
-import { Component, inject } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {Post} from "../../shared/models/post";
+import {PostService} from "../../shared/services/post.service";
+import {Comment} from "../../shared/models/comment";
+import {CommentService} from "../../shared/services/comment.service";
+import {HttpErrorResponse} from "@angular/common/http";
+import {Channel} from "../../shared/models/channel";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['../../app.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+
   route: ActivatedRoute = inject(ActivatedRoute);
   postId: number = this.route.snapshot.params['postId'];
+
+  public post: Post | undefined;
+  public comments: Comment[] = [];
+  constructor(
+    private postService: PostService,
+    private commentService: CommentService
+  ) {}
+
+  ngOnInit() {
+
+
+    this.getPost();
+    this.getComments();
+  }
+  public getComments(): void {
+    this.commentService.getComments(1, this.postId, 1, 10)
+      .subscribe(
+        (comments: Comment[]) => {
+          this.comments =comments;
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error);
+        }
+      )
+
+  }
+  public getPost(): void {
+    this.postService.getPost(1, this.postId)
+      .subscribe(
+      (post) => {
+        this.post = post;
+      },
+      (error) => {
+      }
+    );
+  }
+  public getFormattedChannelName(channel: Channel): string {
+    return Channel.toString(channel);
+  }
+
 }
--- webapp/front/front/src/app/shared/models/channel.ts
+++ webapp/front/front/src/app/shared/models/channel.ts
@@ -4,5 +4,26 @@ export enum Channel {
   FEED = 'FEED',
   WORKERS = 'WORKERS',
   RESERVATIONS = 'RESERVATIONS',
-  INFORMATION = 'INFORMATION'
+  INFORMATION = 'INFORMATION',
+}
+
+export namespace Channel {
+  export function toString(channel: Channel): string {
+    switch (channel) {
+      case Channel.ANNOUNCEMENTS:
+        return 'Announcements';
+      case Channel.COMPLAINTS:
+        return 'Complaints';
+      case Channel.FEED:
+        return 'Feed';
+      case Channel.WORKERS:
+        return 'Workers';
+      case Channel.RESERVATIONS:
+        return 'Reservations';
+      case Channel.INFORMATION:
+        return 'Information';
+      default:
+        return 'Unknown Channel';
+    }
+  }
 }
