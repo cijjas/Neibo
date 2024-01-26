a0bb048301898f581baca91f00b7b195eb84ac78
meursault00 <inakibengolea@gmail.com>
2024-01-25 22:31:52 -0300

    Changes to wave footer component

--- webapp/front/front/src/app/components/wave-footer/wave-footer.component.ts
+++ webapp/front/front/src/app/components/wave-footer/wave-footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-wave-footer',
   templateUrl: './wave-footer.component.html',
-  styleUrl: './wave-footer.component.css'
+  styleUrls: ['../../app.component.css']
 })
 export class WaveFooterComponent {
 
