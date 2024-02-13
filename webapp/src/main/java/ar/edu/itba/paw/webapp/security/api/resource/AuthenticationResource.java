fe8ddaa2e3bf147f57fa19cd39c9a604fbcb42b9
JoacoGirod <joaquingirod@gmail.com>
2024-02-13 17:37:38 -0300

    Add custom header for auth

--- webapp/src/main/java/ar/edu/itba/paw/webapp/security/api/resource/AuthenticationResource.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/security/api/resource/AuthenticationResource.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.webapp.security.api.resource;
 
 import ar.edu.itba.paw.enums.Authority;
+import ar.edu.itba.paw.webapp.auth.UserAuth;
 import ar.edu.itba.paw.webapp.form.LoginForm;
 import ar.edu.itba.paw.webapp.security.api.AuthenticationTokenDetails;
 import ar.edu.itba.paw.webapp.security.api.model.AuthenticationToken;
@@ -15,8 +16,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -31,6 +34,9 @@ public class AuthenticationResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationResource.class);
 
+    @Context
+    private UriInfo uriInfo;
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -38,12 +44,12 @@ public class AuthenticationResource {
     private AuthenticationTokenService authenticationTokenService;
 
     /*
-    * Security Context is constantly used even though it seems like something more commonly used in stateful implementations
-    * this is because it allows for SSOT across the multiple layers that are involved in the authentication, the JWT Token Filter
-    * which is the first thing that processes each HTTP request is the one in charge of filling it for the first time, due to
-    * that all the methods like the ones in this class can safely use the Security Context knowing this is not conflicting
-    * with other clients' requests, as the context is unique per request
-    */
+     * Security Context is constantly used even though it seems like something more commonly used in stateful implementations
+     * this is because it allows for SSOT across the multiple layers that are involved in the authentication, the JWT Token Filter
+     * which is the first thing that processes each HTTP request is the one in charge of filling it for the first time, due to
+     * that all the methods like the ones in this class can safely use the Security Context knowing this is not conflicting
+     * with other clients' requests, as the context is unique per request
+     */
 
     /**
      * Validate user credentials and issue a token for the user.
@@ -75,7 +81,15 @@ public class AuthenticationResource {
         AuthenticationToken authenticationToken = new AuthenticationToken();
         authenticationToken.setToken(token);
 
-        return Response.ok(authenticationToken).build();
+        // Get neighborhoodId and userId to build the URN
+        UserAuth userAuth = (UserAuth) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String urn = String.format("%s/neighborhoods/%d/users/%d", uriInfo.getBaseUri().toString(),userAuth.getNeighborhoodId(), userAuth.getUserId());
+
+        // Add URN to the response header
+        return Response.ok(authenticationToken)
+                .header("X-User-URN", urn)
+                .build();
     }
 
     /**
@@ -96,6 +110,14 @@ public class AuthenticationResource {
         AuthenticationToken authenticationToken = new AuthenticationToken();
         authenticationToken.setToken(token);
 
-        return Response.ok(authenticationToken).build();
+        // Get neighborhoodId and userId to build the URN
+        UserAuth userAuth = (UserAuth) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String urn = String.format("%s/neighborhoods/%d/users/%d", uriInfo.getBaseUri().toString(),userAuth.getNeighborhoodId(), userAuth.getUserId());
+
+        // Add URN to the response header
+        return Response.ok(authenticationToken)
+                .header("X-User-URN", urn)
+                .build();
     }
 }
